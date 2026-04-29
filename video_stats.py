74870472d72025580e3b50108929b76233c154ef
Rodrigo Lopez Rosales <roy_006@hotmail.com>
2026-04-29 11:22:13 -0600

    Final develop change

--- video_stats.py
+++ video_stats.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path="./.env")
 
@@ -92,7 +93,16 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
+def save_to_jason(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path,'w',encoding='utf-8') as json_outfile:
+        json.dump(extracted_data,json_outfile,indent=4, ensure_ascii=False)
+
+
 if __name__  == "__main__":
    playlistId = get_playlist_id()
    videosListId = get_video_ids(playlistId)
-   print(extract_video_data(videosListId))
+   video_data=extract_video_data(videosListId)
+   save_to_jason(video_data)
+
